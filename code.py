3c4a256ba38698a6dd46194ce86ced3e0f006285
akhil118 <gandikota.abai@gmail.com>
2022-06-06 12:57:56 +0530

    updated code according to flake 8

--- code.py
+++ code.py
@@ -1,317 +1,322 @@
 import csv
-from importlib.resources import path
-from optparse import Values
+
+
 import matplotlib.pyplot as plt
 
 
 def rawdata(filename):  # collecting the .csv files and converting into data
-    with open(filename,"r") as file :
-        csvReader=csv.DictReader(file)
-        data=list(csvReader)
-        
+    with open(filename, "r") as file:
+        csvReader = csv.DictReader(file)
+        data = list(csvReader)
     return data
-              
-    
 
-def totalRunsByEachTeam(deliveriesCsv):#collecting the data from "deliveries.csv" and dividing the total runs of each team
-    matchdata=rawdata(deliveriesCsv)  # importing the file of deliveries.csv and converting them into data
-    #details=matchdata.pop(0)
-    teams=[]
-    for team in matchdata :
+
+def totalRunsByEachTeam(deliveriesCsv):
+    # collecting the data from "deliveries.csv" and
+    # dividing the total runs of each team
+
+    # importing the file of deliveries.csv and converting them into data
+    matchdata = rawdata(deliveriesCsv)
+    # details=matchdata.pop(0)
+    teams = []
+    for team in matchdata:
         teams.append(team["batting_team"])
-    teams=list(set(teams))
-    #print(teams)
-    teamscores=[0]*len(teams)
+    teams = list(set(teams))
+    # print(teams)
+    teamscores = [0]*len(teams)
 
-    teamsDict=dict(zip(teams,teamscores))
+    teamsDict = dict(zip(teams, teamscores))
 
-    #print(data)
-    #print(details[17])
+    # print(data)
+    # print(details[17])
 
-    for team in matchdata :
-        teamsDict[team["batting_team"]]=teamsDict[team["batting_team"]]+int(team["total_runs"])
-    
-    plt.bar(teamsDict.keys(),teamsDict.values())
+    for team in matchdata:
+        battingTeam = team["batting_team"]
+        teamsDict[battingTeam] = teamsDict[battingTeam]+int(team["total_runs"])
+    plt.bar(teamsDict.keys(), teamsDict.values())
     plt.show()
-    
-def topBatsmanOfRCB(deliveriesCsv) : #collecting data from "deliveries.csv" and finding the player scored topruns for RCB in entire game
-    matchdata=rawdata(deliveriesCsv)  #importing the file of deliveries.csv and converting them into data
-    RCBdata=[]
-    
-    for team in matchdata :
-        
-       if team["batting_team"]=="Royal Challengers Bangalore" :
-           
-           RCBdata.append(team)
-    players=[]   
-    for player in RCBdata :
-        
+
+
+def topBatsmanOfRCB(deliveriesCsv):
+    # collecting data from "deliveries.csv" and
+    # finding the playerscored topruns for RCB in entire game
+
+    # importing the file of deliveries.csv and converting them into data
+    matchdata = rawdata(deliveriesCsv)
+    RCBdata = []
+
+    for team in matchdata:
+
+        if team["batting_team"] == "Royal Challengers Bangalore":
+
+            RCBdata.append(team)
+    players = []
+    for player in RCBdata:
+
         players.append(player["batsman"])
-        
-    players=list(set(players))
-    
-    runs=[0]*len(players)
-    
-    
-    playerdata=dict(zip(players,runs))
-    
-    
-    for runs in RCBdata :
-        playerdata[runs["batsman"]]=playerdata[runs["batsman"]]+int(runs["total_runs"])
-        
-    
-    
- 
-    plt.bar(playerdata.keys(),playerdata.values())
+
+    players = list(set(players))
+
+    runs = [0]*len(players)
+
+    playerdata = dict(zip(players, runs))
+
+    for runs in RCBdata:
+        batsman = runs["batsman"]
+        playerdata[batsman] = playerdata[batsman]+int(runs["total_runs"])
+
+    plt.bar(playerdata.keys(), playerdata.values())
     plt.show()
 
-def umpireAnalysisChart(umpairesCsv) : # collecting the data from "umpires.csv" and finding the barchart of umpires on basis of country and no of umpires per country
-    umpiresdata=rawdata(umpairesCsv)  #importing the file of umpires.csv and converting them into data
-    countries=[]
-    
-    for data in umpiresdata:
-        
-        countries.append(data["Nationality"].strip())
-        
-    uniqueCountries=list(set(countries))
-    
-    uniqueCountries.remove("India")
-    #uniqueCountries.remove("Nationality")
-    #print(uniqueCountries)
-    
-    nums=[0]*len(uniqueCountries)
 
-    countriesdata=dict(zip(uniqueCountries,nums))
-    
-    
-    for data in uniqueCountries :
-        
-        countriesdata[data]=countries.count(data)
-        
-   
-    #print(countriesdata)
-    
+def umpireAnalysisChart(umpairesCsv):
+    # collecting the data from "umpires.csv" and
+    # getting barchart of umpires on basis of country and no of umpires
+
+    # importing the file of umpires.csv and converting them into data
+    umpiresdata = rawdata(umpairesCsv)
+    countries = []
+
+    for data in umpiresdata:
+
+        countries.append(data["Nationality"].strip())
+
+    uniqueCountries = list(set(countries))
+
+    uniqueCountries.remove("India")
+    # uniqueCountries.remove("Nationality")
+    # print(uniqueCountries)
+
+    nums = [0]*len(uniqueCountries)
+
+    countriesdata = dict(zip(uniqueCountries, nums))
+
+    for data in uniqueCountries:
+
+        countriesdata[data] = countries.count(data)
+
+    # print(countriesdata)
+
     plt.bar(countriesdata.keys(), countriesdata.values())
     plt.show()
-    
-def StackedchartOfMatchesPlayedByTeamAndBySeason(matchesCsv): # collecting data from "matches.csv" file and finding StackedchartOfMatchesPlayedByTeamAndBySeason
-    matchsdata=rawdata(matchesCsv)
-    years=[]
-    teams=[]
-    for season in matchsdata :
+
+
+def StackedchartOfMatchesPlayedByTeamAndBySeason(matchesCsv):
+    # collecting data from "matches.csv" file and
+    # finding StackedchartOfMatchesPlayedByTeamAndBySeason
+    matchsdata = rawdata(matchesCsv)
+    years = []
+    teams = []
+    for season in matchsdata:
         years.append(season["season"])
         teams.append(season["team1"])
-    years=sorted(list(set(years)))
-    teams=sorted(list(set(teams)))
-  
-    values=[0]*len(years)
-    
-    nums=[0]*len(teams)
-    
-    dataYears=dict(zip(years,values))
-    dataTeams=dict(zip(teams,nums))
-    
-    
-    #print(dataTeams)
+    years = sorted(list(set(years)))
+    teams = sorted(list(set(teams)))
+
+    values = [0]*len(years)
+
+    nums = [0]*len(teams)
+
+    dataYears = dict(zip(years, values))
+    dataTeams = dict(zip(teams, nums))
+
+    # print(dataTeams)
 
     for matches in matchsdata:
-        dataYears[matches["season"]]=dataYears[matches["season"]]+1
-        dataTeams[matches["team1"]]=dataTeams[matches["team1"]]+1
-        
+        dataYears[matches["season"]] = dataYears[matches["season"]]+1
+        dataTeams[matches["team1"]] = dataTeams[matches["team1"]]+1
+
     # dataYears.pop("season")
     # dataTeams.pop("team1")
-    
-        
-    
-    
-   
-    plt.bar(dataYears.keys(),dataYears.values())
+
+    plt.bar(dataYears.keys(), dataYears.values())
     plt.show()
-    plt.bar(dataTeams.keys(),dataTeams.values())
+    plt.bar(dataTeams.keys(), dataTeams.values())
     plt.show()
 
 
-def NumberOfMatchesPlayedPerYearForAllTheYearsInIPL(matchesCsv):
-    matchdata=rawdata(matchesCsv)  # importing the file of matches.csv and converting them into data
-    years=[]
-    
-    for year in matchdata :
-        
+def NoOfMatchesPlayedPerYear(matchesCsv):
+    # collecting data from "matches.csv" file and
+    # finding no of matches played per year
+
+    # importing the file of matches.csv and converting them into data
+    matchdata = rawdata(matchesCsv)
+    years = []
+
+    for year in matchdata:
+
         years.append(year["season"])
-        
-    years=sorted(list(set(years)))
-    
-    #print(years)
-    
-    matches=[0]*len(years)
-    
-    data=dict(zip(years,matches))
-    
-    #print(data)
-    
-    
-    for matches in matchdata :
-        
-        data[matches["season"]]=data[matches["season"]] +1
-        
+
+    years = sorted(list(set(years)))
+
+    # print(years)
+
+    matches = [0]*len(years)
+
+    data = dict(zip(years, matches))
+
+    # print(data)
+
+    for matches in matchdata:
+
+        data[matches["season"]] = data[matches["season"]] + 1
+
     print(data)
-    #return(data)   #using for test case
-    
-def NumberOfMatchesWonPerTeamPerYearInIPL(matchesCsv) :
-    
-    matchdata=rawdata(matchesCsv)  # importing the file of maatches.csv and converting them into data
-    years=[]
-    teams=[]
-    
-    for data in matchdata :
-        
+    # return(data)   #using for test case
+
+
+def NumberOfMatchesWonPerTeamPerYearInIPL(matchesCsv):
+    # collecting data from "matches.csv" file and
+    # finding no of matches won per year by team
+
+    # importing the file of maatches.csv and converting them into data
+    matchdata = rawdata(matchesCsv)
+    years = []
+    teams = []
+
+    for data in matchdata:
+
         years.append(data["season"])
         teams.append(data["team1"])
-            
-    years=sorted(list(set(years)))
-    teams=list(set(teams))
 
-    
-    
-    #print(teams,years)
-    values=[0]*len(teams)
-    teamsPerYear=dict(zip(teams,values))
-    teamValue=[teamsPerYear]*len(years)
-    totalTeamsPerYear=dict(zip(years,teamValue))
-    
-    #print(totalTeamsPerYear)
-    
-    
-    for data in matchdata :
-        year=data["season"]
-        winner=data["winner"]
-        try :
-            totalTeamsPerYear[year][winner] += 1
-        except:
-            pass 
-    
-    
+    years = sorted(list(set(years)))
+    teams = list(set(teams))
+
+    # print(teams,years)
+    values = [0]*len(teams)
+    teamsPerYear = dict(zip(teams, values))
+    teamValue = [teamsPerYear]*len(years)
+    totalTeamsPerYear = dict(zip(years, teamValue))
+
     print(totalTeamsPerYear)
-    #return(list(totalTeamsPerYear.values())[0])    #using for test case
-    
-    
-    
-def ExtraRunsConcededPerTeamInTheYear2016(deliveriesCsv,matchesCsv) :
-    
-    deliveriesdata=rawdata(deliveriesCsv)  # importing the file of deliveries.csv and converting them into data
-    
-    matchdata=rawdata(matchesCsv) 
-    teams=[]
-    
-    for data in deliveriesdata :    
+
+    for data in matchdata:
+        year = data["season"]
+        winner = data["winner"]
+        try:
+            totalTeamsPerYear[year][winner] += 1
+
+        except Exception:
+            pass
+
+    print(totalTeamsPerYear)
+    # return(list(totalTeamsPerYear.values())[0])    #using for test case
+
+
+def ExtraRunsPerTeamIn2016(deliveriesCsv, matchesCsv):
+    # collecting data from "matches.csv" and "deliveries.csv" files and
+    # finding the extra runs conceded per team in 2016
+
+    # importing the file of deliveries.csv and converting them into data
+    deliveriesdata = rawdata(deliveriesCsv)
+
+    matchdata = rawdata(matchesCsv)
+    teams = []
+
+    for data in deliveriesdata:
         teams.append(data["batting_team"])
-            
-    
-    teams=list(set(teams))
 
-    values=[0]*len(teams)
+    teams = list(set(teams))
 
-    teamsWithRuns=dict(zip(teams,values))
-    
-    id=[]
-    for ids in matchdata :
-        
-        if ids["season"]=="2016" :
+    values = [0]*len(teams)
+
+    teamsWithRuns = dict(zip(teams, values))
+
+    id = []
+    for ids in matchdata:
+
+        if ids["season"] == "2016":
             id.append(ids["id"])
-            
-    #print(id)
-    
-    
-    
-    for data in deliveriesdata :
-        
-        if data["match_id"] in id :
-            teamsWithRuns[data["batting_team"]]=teamsWithRuns[data["batting_team"]]+int(data["extra_runs"])
-            
+
+    # print(id)
+
+    for data in deliveriesdata:
+
+        if data["match_id"] in id:
+            teamsWithRuns[data["batting_team"]] += int(data["extra_runs"])
+
     print(teamsWithRuns)
-    #return teamsWithRuns       #using for test case
-    
-def Top10EconomicalBowlerInTheYear2015(deliveriesCsv,matchesCsv) :
-    deliveriesdata=rawdata(deliveriesCsv)  # importing the file of deliveries.csv and converting them into data
-    matchdata=rawdata(matchesCsv) 
-   
-    
-    id=[]
-    for ids in matchdata :
-        
-        if ids["season"]=="2015" :
+    # return teamsWithRuns       #using for test case
+
+
+def Top10EconomicalBowlerIn2015(deliveriesCsv, matchesCsv):
+    # collecting data from "matches.csv" and "deliveries.csv" files and
+    # finding top economical bowler in 2015
+
+    # importing the file of deliveries.csv and converting them into data
+    deliveriesdata = rawdata(deliveriesCsv)
+    matchdata = rawdata(matchesCsv)
+
+    id = []
+    for ids in matchdata:
+
+        if ids["season"] == "2015":
             id.append(ids["id"])
-            
-    bowlers=[]
-    
-    for data in deliveriesdata :    
-        
-        if data["match_id"] in id :
+
+    bowlers = []
+
+    for data in deliveriesdata:
+
+        if data["match_id"] in id:
             bowlers.append(data["bowler"])
-            
-    
-    bowlers=list(set(bowlers))
 
-    values=[0]*len(bowlers)
+    bowlers = list(set(bowlers))
 
-    bowlersWithRuns=dict(zip(bowlers,values))
-    bowlersWithOvers=dict(zip(bowlers,values))
-    
-    for data in deliveriesdata :
-        
-        if data["match_id"] in id :
-            bowlersWithRuns[data["bowler"]]=bowlersWithRuns[data["bowler"]]+int(data["total_runs"])
-            bowlersWithOvers[data["bowler"]]=bowlersWithOvers[data["bowler"]] +1
-            
-    #print(bowlersWithOvers,bowlersWithRuns)
-    #print(len(bowlersWithOvers.keys()),len(bowlersWithRuns.keys()))
+    values = [0]*len(bowlers)
 
+    bowlersWithRuns = dict(zip(bowlers, values))
+    bowlersWithOvers = dict(zip(bowlers, values))
 
-    
+    for data in deliveriesdata:
+
+        if data["match_id"] in id:
+            bowlersWithRuns[data["bowler"]] += int(data["total_runs"])
+            bowlersWithOvers[data["bowler"]] += 1
+
+    # print(bowlersWithOvers,bowlersWithRuns)
+    # print(len(bowlersWithOvers.keys()),len(bowlersWithRuns.keys()))
+
     totalBowlers = list(bowlersWithRuns.keys())
-    
-    totalRunsByBowler= list(bowlersWithRuns.values())
-    totalOversByBowler=list(bowlersWithOvers.values())
-    
-    
-    bowlerEconomyList=[]
-    count=0
-    for bowler in totalBowlers :
-        
-        economy=(totalRunsByBowler[count]/totalOversByBowler[count])*6
-        bowlerDetails=[economy,bowler]
+
+    totalRunsByBowler = list(bowlersWithRuns.values())
+    totalOversByBowler = list(bowlersWithOvers.values())
+
+    bowlerEconomyList = []
+    count = 0
+    for bowler in totalBowlers:
+
+        economy = (totalRunsByBowler[count]/totalOversByBowler[count])*6
+        bowlerDetails = [economy, bowler]
         bowlerEconomyList.append(bowlerDetails)
-        count=count+1
-        
-    top10EconomyBowlersList=sorted(bowlerEconomyList)[:10]
-    
-    top10EconomyBowlers ={}
-    
-    for bowlers in top10EconomyBowlersList :
-        
-        top10EconomyBowlers[bowlers[-1]]=bowlers[0]
-        
+        count = count+1
+
+    top10EconomyBowlersList = sorted(bowlerEconomyList)[:10]
+
+    top10EconomyBowlers = {}
+
+    for bowlers in top10EconomyBowlersList:
+
+        top10EconomyBowlers[bowlers[-1]] = bowlers[0]
+
     print(top10EconomyBowlers)
-        
-    #return top10EconomyBowlers     #using for test case
+
+    # return top10EconomyBowlers     #using for test case
+
 
 def main():
-    
-    matchesPath="/home/akhil118/Desktop/git/IPL-data-set-analytics/matches.csv"
-    deliveriesPath="/home/akhil118/Desktop/git/IPL-data-set-analytics/deliveries.csv"
-    umpairesPath="/home/akhil118/Desktop/git/IPL-data-set-analytics/umpires.csv"
-    
+
+    matchesPath = "/home/akhil118/Desktop/git/IPL /matches.csv"
+    deliveriesPath = "/home/akhil118/Desktop/git/IPL /deliveries.csv"
+    umpairesPath = "/home/akhil118/Desktop/git/IPL /umpires.csv"
+
     totalRunsByEachTeam(deliveriesPath)
     topBatsmanOfRCB(deliveriesPath)
     umpireAnalysisChart(umpairesPath)
     StackedchartOfMatchesPlayedByTeamAndBySeason(matchesPath)
-    NumberOfMatchesPlayedPerYearForAllTheYearsInIPL(matchesPath)
+    NoOfMatchesPlayedPerYear(matchesPath)
     NumberOfMatchesWonPerTeamPerYearInIPL(matchesPath)
-    ExtraRunsConcededPerTeamInTheYear2016(deliveriesPath,matchesPath)
-    Top10EconomicalBowlerInTheYear2015(deliveriesPath,matchesPath)
+    ExtraRunsPerTeamIn2016(deliveriesPath, matchesPath)
+    Top10EconomicalBowlerIn2015(deliveriesPath, matchesPath)
 
-
-
-    
 
 main()
--- testCase.py
+++ testCase.py
@@ -1,98 +1,111 @@
 import unittest
 import matplotlib.pyplot as plt
-import code 
+import code
 import numpy as np
 
 
+mockMatchesPath = "/home/akhil118/Desktop/git/IPL/mock_matches.csv"
+mockDelPath = "/home/akhil118/Desktop/git/IPL/mock_deliveries.csv"
 
 
-class testcode(unittest.TestCase) :
-    
+class testcode(unittest.TestCase):
+
     def test_1st(self):
-        
-        
-        test1={'2008': 1, '2009': 2, '2010': 1, '2011': 3, '2012': 1, '2013': 4, '2014': 3, '2015': 1, '2016': 1, '2017': 2}
-        #test2={'2008': 1, '2009': 4, '2010': 3, '2011': 3, '2012': 1, '2013': 3, '2014': 4, '2015': 1, '2016': 1, '2017': 2}
-        result=code.NumberOfMatchesPlayedPerYearForAllTheYearsInIPL("/home/akhil118/Desktop/git/IPL-data-set-analytics/mock_matches.csv")
-        self.assertEqual(result,test1)
-        #self.assertEqual(result,test2)
-         
-        x=list(result.keys())
-        y1=list(result.values())
-        y2=list(test1.values())
-        data=[y1,y2]
+
+        test1 = {'2008': 1, '2009': 2, '2010': 1, '2011': 3, '2012': 1,
+                 '2013': 4, '2014': 3, '2015': 1, '2016': 1, '2017': 2}
+        # test2 = {'2008': 1, '2009': 4, '2010': 3, '2011': 3, '2012': 1,
+        #          '2013': 3, '2014': 4, '2015': 1, '2016': 1, '2017': 2}
+        result = code.NoOfMatchesPlayedPerYear(mockMatchesPath)
+        self.assertEqual(result, test1)
+        # self.assertEqual(result,test2)
+
+        x = list(result.keys())
+        y1 = list(result.values())
+        y2 = list(test1.values())
+        data = [y1, y2]
         X = np.arange(len(x))
-        plt.bar(X + 0.00, data[0], color = 'b', width = 0.25)
-        plt.bar(X + 0.25, data[1], color = 'g', width = 0.25)
-        
+        plt.bar(X + 0.00, data[0], color='b', width=0.25)
+        plt.bar(X + 0.25, data[1], color='g', width=0.25)
+
         plt.show()
-        
-       
-        
+
     def test_2nd(self):
-         
-        #return(list(totalTeamsPerYear.values())[0])
-        test1={'Sunrisers Hyderabad': 3, 'Deccan Chargers': 1, 'Gujarat Lions': 1, 'Mumbai Indians': 1, 'Kings XI Punjab': 0, 'Kochi Tuskers Kerala': 0, 'Rajasthan Royals': 3, 'Kolkata Knight Riders': 3, 'Royal Challengers Bangalore': 4}
-        #test2={'Sunrisers Hyderabad': 3, 'Deccan Chargers': 2, 'Gujarat Lions': 1, 'Mumbai Indians': 1, 'Kings XI Punjab': 0, 'Kochi Tuskers Kerala': 0, 'Rajasthan Royals': 3, 'Kolkata Knight Riders': 3, 'Royal Challengers Bangalore': 4}
-        result=code.NumberOfMatchesWonPerTeamPerYearInIPL("/home/akhil118/Desktop/git/IPL-data-set-analytics/mock_matches.csv")
-        self.assertEqual(result,test1)
-        #self.assertEqual(result,test2)
-        
-        
-        
-        x=list(result.keys())
-        y1=list(result.values())
-        y2=list(test1.values())
-        data=[y1,y2]
+
+        # return(list(totalTeamsPerYear.values())[0])
+        test1 = {'Sunrisers Hyderabad': 3, 'Deccan Chargers': 1,
+                 'Gujarat Lions': 1, 'Mumbai Indians': 1, 'Kings XI Punjab': 0,
+                 'Kochi Tuskers Kerala': 0, 'Rajasthan Royals': 3,
+                 'Kolkata Knight Riders': 3, 'Royal Challengers Bangalore': 4}
+        # test2 = {'Sunrisers Hyderabad': 3, 'Deccan Chargers': 2,
+        #          'Gujarat Lions': 1, 'Mumbai Indians':1,
+        #          'Kings XI Punjab': 0,
+        #          'Kochi Tuskers Kerala': 0, 'Rajasthan Royals': 3,
+        #          'Kolkata Knight Riders': 3,
+        #          'Royal Challengers Bangalore': 4}
+
+        result = code.NumberOfMatchesWonPerTeamPerYearInIPL(mockMatchesPath)
+        self.assertEqual(result, test1)
+        # self.assertEqual(result, test2)
+
+        x = list(result.keys())
+        y1 = list(result.values())
+        y2 = list(test1.values())
+        data = [y1, y2]
         X = np.arange(len(x))
-        plt.bar(X + 0.00, data[0], color = 'b', width = 0.25)
-        plt.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+        plt.bar(X + 0.00, data[0], color='b', width=0.25)
+        plt.bar(X + 0.25, data[1], color='g', width=0.25)
         plt.show()
-        
+
     def test_3rd(self):
-             
-        #return(list(totalTeamsPerYear.values())[0])
-        test1={'Mumbai Indians': 0, 'Royal Challengers Bangalore': 0, 'Delhi Daredevils': 1, 'Gujarat Lions': 3}
-        #test2={'Mumbai Indians': 2, 'Royal Challengers Bangalore': 1, 'Delhi Daredevils': 1, 'Gujarat Lions': 3}
 
-        result=code.ExtraRunsConcededPerTeamInTheYear2016("/home/akhil118/Desktop/git/IPL-data-set-analytics/mock_deliveries.csv" ,"/home/akhil118/Desktop/git/IPL-data-set-analytics/mock_matches.csv")
-        self.assertEqual(result,test1)
-        #self.assertEqual(result,test2)
-        
-        
-        
-        x=list(result.keys())
-        y1=list(result.values())
-        y2=list(test1.values())
-        data=[y1,y2]
+        # return(list(totalTeamsPerYear.values())[0])
+        test1 = {'Mumbai Indians': 0, 'Royal Challengers Bangalore': 0,
+                 'Delhi Daredevils': 1, 'Gujarat Lions': 3}
+        # test2 = {'Mumbai Indians': 2, 'Royal Challengers Bangalore': 1,
+        #          'Delhi Daredevils': 1, 'Gujarat Lions': 3}
+
+        result = code.ExtraRunsPerTeamIn2016(mockDelPath, mockMatchesPath)
+        self.assertEqual(result, test1)
+        # self.assertEqual(result, test2)
+
+        x = list(result.keys())
+        y1 = list(result.values())
+        y2 = list(test1.values())
+        data = [y1, y2]
         X = np.arange(len(x))
-        plt.bar(X + 0.00, data[0], color = 'b', width = 0.25)
-        plt.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+        plt.bar(X + 0.00, data[0], color='b', width=0.25)
+        plt.bar(X + 0.25, data[1], color='g', width=0.25)
         plt.show()
-        
+
     def test_4th(self):
-             
-        #return(list(totalTeamsPerYear.values())[0])
-        test1={'SL Malinga': 0.0, 'D Wiese': 5.142857142857142, 'JJ Bumrah': 7.714285714285715, 'J Suchith': 10.0, 'S Aravind': 10.5, 'HV Patel': 10.8, 'MJ McClenaghan': 13.0, 'MA Starc': 14.399999999999999, 'Harbhajan Singh': 15.0, 'YS Chahal': 16.0}
-        #test2={'SL Malinga': 3.5, 'D Wiese': 5.142857142857, 'JJ Bumrah': 7.714285714285715, 'J Suchith': 1.0, 'S Aravind': 1.5, 'HV Patel': 10.8, 'MJ McClenaghan': 13.0, 'MA Starc': 14.399999999999999, 'Harbhajan Singh': 15.0, 'YS Chahal': 16.0}
 
-        result=code.Top10EconomicalBowlerInTheYear2015("/home/akhil118/Desktop/git/IPL-data-set-analytics/mock_deliveries.csv" ,"/home/akhil118/Desktop/git/IPL-data-set-analytics/mock_matches.csv")      
-        self.assertEqual(result,test1)
-        #self.assertEqual(result,test2)
-        
-        
-        
-        x=list(result.keys())
-        y1=list(result.values())
-        y2=list(test1.values())
-        data=[y1,y2]
+        # return(list(totalTeamsPerYear.values())[0])
+        test1 = {'SL Malinga': 0.0, 'D Wiese': 5.142857142857142,
+                 'JJ Bumrah': 7.714285714285715, 'J Suchith': 10.0,
+                 'S Aravind': 10.5, 'HV Patel': 10.8, 'MJ McClenaghan': 13.0,
+                 'MA Starc': 14.399999999999999, 'Harbhajan Singh': 15.0,
+                 'YS Chahal': 16.0}
+        # test2 = {'SL Malinga': 3.5, 'D Wiese': 5.142857142857,
+        #          'JJ Bumrah': 7.714285714285715, 'J Suchith': 1.0,
+        #          'S Aravind': 1.5, 'HV Patel': 10.8, 'MJ McClenaghan': 13.0,
+        #          'MA Starc': 14.399999999999999, 'Harbhajan Singh': 15.0,
+        #          'YS Chahal': 16.0}
+
+        result = code.Top10EconomicalBowlerIn2015(mockDelPath, mockMatchesPath)
+        self.assertEqual(result, test1)
+        # self.assertEqual(result, test2)
+
+        x = list(result.keys())
+        y1 = list(result.values())
+        y2 = list(test1.values())
+        data = [y1, y2]
         X = np.arange(len(x))
-        plt.bar(X + 0.00, data[0], color = 'b', width = 0.25)
-        plt.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+        plt.bar(X + 0.00, data[0], color='b', width=0.25)
+        plt.bar(X + 0.25, data[1], color='g', width=0.25)
         plt.show()
-        
-        
-        
-if __name__=="__main__" :
-    
+
+
+if __name__ == "__main__":
+
     unittest.main()
